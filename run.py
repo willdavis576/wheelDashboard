ed7a213ac8d139453a0719e0177aea174794c81e
Will Davis <willdavis576@gmail.com>
2021-10-19 22:48:37 +0100

    buttons for rpi

--- run.py
+++ run.py
@@ -1,17 +1,28 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow
-import sys
+# from PyQt5.QtWidgets import QApplication, QMainWindow
+# import sys
 
 
-class Window(QMainWindow):
-    def __init__(self):
-        super().__init__()
+# class Window(QMainWindow):
+#     def __init__(self):
+#         super().__init__()
 
-        # self.setGeometry(300, 300, 720, 720)
-        self.showMaximized()
-        self.setWindowTitle("PyQt5 window")
-        self.show()
+#         self.setGeometry(0, 0, 720, 720)
+#         self.setWindowTitle("PyQt5 window")
+#         self.show()
 
 
-app = QApplication(sys.argv)
-window = Window()
-sys.exit(app.exec_())
+# app = QApplication(sys.argv)
+# window = Window()
+# sys.exit(app.exec_())
+
+
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
+app = QApplication([])
+window = QWidget()
+layout = QVBoxLayout()
+layout.addWidget(QPushButton('Top'))
+layout.addWidget(QPushButton('Bottom'))
+window.setLayout(layout)
+window.setGeometry(0,0,720,720)
+window.show()
+app.exec()
