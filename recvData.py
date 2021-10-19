09feb30c460397a42f1efbb6ff185961491aad13
Will Davis <william.davis@live.co.uk>
2021-10-19 22:04:58 +0100

    initial commit

---
+++ recvData.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+from socket import *
+from struct import *
+from IPython import embed
+
+serverName='192.168.0.163'
+serverPort = 9996
+clientSocket = socket(AF_INET,SOCK_DGRAM)
+message=pack('iii',1,1,0)
+
+clientSocket.sendto(message, (serverName, serverPort))
+
+messageback, ipAddress=clientSocket.recvfrom(1024)
+print( 'received')
+pktformat='100s100sii100s100s'
+n = calcsize(pktformat)
+print (n)
+print (unpack(pktformat, messageback))
+
+backString = unpack(pktformat, messageback)
+
+carName = backString[0].decode(errors='ignore').replace('\x00', '').split('%')[0]
+driverName = backString[1].decode(errors='ignore').replace('\x00', '').split('%')[0]
+trackName = backString[4].decode(errors='ignore').replace('\x00', '').split('%')[0]
+trackConfig = backString[5].decode(errors='ignore').replace('\x00', '').split('%')[0]
+
+print(carName, driverName, trackName)
+
+message=pack('iii',1,1,1)
+clientSocket.sendto(message, (serverName, serverPort))
+
+i=0
+
+
+while 1:
+    pktformat='cifff??????fffiiiifffffiffffffffffffffffff'
+    messageback, ipAddress =clientSocket.recvfrom(16384)
+    backString = unpack(pktformat, messageback[:152])
+    print (backString[:7])
+
+    gear = backString[23]
+    speed = backString[3]
+    #
+
+
+
+
+    i+=1
+
+
+
+clientSocket.close()
---
+++ run.py
@@ -0,0 +1,16 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
+
+
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setGeometry(300, 300, 720, 720)
+        self.setWindowTitle("PyQt5 window")
+        self.show()
+
+
+app = QApplication(sys.argv)
+window = Window()
+sys.exit(app.exec_())
